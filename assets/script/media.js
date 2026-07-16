8b8d8edcf4ab7f090c666c5c48f687d2e8048d0d
Bence <51375275+BenceBarens@users.noreply.github.com>
2026-07-16 11:08:30 +0200

    Added the base for portfolio home page

---
+++ assets/script/media.js
@@ -0,0 +1,39 @@
+// Website Made by Bence (bencebarens.nl)
+async function loadPhotos() {
+  try {
+    const response = await fetch('./media.json');
+    const photoFiles = await response.json();
+    const listContainer = document.getElementById('photo-grid');
+
+    const imageWidth = 200;       // Maximale breedte in pixels
+    const imageQuality = 50;      // Kwaliteit (tussen 1 en 100)
+    const imageFormat = 'webp';   // Output formaat (webp, png, jpg)
+
+    photoFiles.forEach(relativeFilePath => {
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      const span = document.createElement('span');
+
+      const rawGithubUrl = `https://raw.githubusercontent.com/BenceBarens/xolunar/main/assets/img/Photo/Photos/${relativeFilePath}`;
+
+      img.src = `https://wsrv.nl/?url=${encodeURIComponent(rawGithubUrl)}&w=${imageWidth}&output=${imageFormat}&q=${imageQuality}`;
+      
+      img.alt = `Portfolio: ${relativeFilePath}`;
+      img.loading = 'lazy';
+
+      const fileName = relativeFilePath.split('/').pop(); 
+      
+      span.textContent = fileName;
+      span.classList.add('photo-title');
+
+      li.appendChild(img);
+      li.appendChild(span);
+      listContainer.appendChild(li);
+    });
+
+  } catch (error) {
+    console.error("Fout bij het laden van de geoptimaliseerde foto's:", error);
+  }
+}
+
+loadPhotos();
